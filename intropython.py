29aa61b4d2a559eded1c109a23387dea67c7afb2
viveks5898 <39966959+viveks5898@users.noreply.github.com>
2018-06-19 10:05:33 -0700

    1st assignment

---
+++ intropython.py
@@ -0,0 +1,26 @@
+#Q.1- Print anything you want on screen
+print("Hello This Is Arijit Singh")
+
+#Q.2- Join two strings using '+'.
+#E.g.-"Acad"+"View”
+print("Acad"+"View")
+
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+a=input("enter a number")
+b=input("enter b number")
+c=input("enter c number")
+print(a,b,c)
+
+#Q.4- Print “Let’s get started” on screen.
+print('Let\'s get started')
+
+#Q.5- Print the following values using placeholders.
+s="Acadview"
+course="Python"
+fees=5000
+print("Hello welcome to %s your course name is %s and total amount pending Rs %d"%(s,course,fees))
+
+#Q.6- Let’s do some interesting exercise:
+name="Tony Stark"
+salary=1000000
+print('%s''%d'%(name,salary))
